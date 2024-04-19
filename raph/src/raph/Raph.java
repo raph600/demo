e3744501eae10c52facadc734f865dfc5e34d299
raphael <raphaelmawele56@gmail.com>
2024-04-19 11:58:27 +0200

    ok

---
+++ raph/src/raph/Raph.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package raph;
+
+/**
+ *
+ * @author hp
+ */
+public class Raph {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       int nbr1= 5;
+       int nbr2= 7;
+       double resultat= nbr1 + nbr2;
+       
+       System.out.println("le resultat est =" + resultat);
+    }
+    
+}
